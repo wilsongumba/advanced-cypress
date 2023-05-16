a1cc0a21182248ea6ea5136965958e312c6eec00
wilsongumba <wilsongumba@gmail.com>
2023-05-17 01:44:03 +0300

    cookies

--- cypress/integration/cookies_start.js
+++ cypress/integration/cookies_start.js
@@ -2,14 +2,43 @@
 
 describe('Cookies', () => {
 
-  beforeEach(() => {
-
+  before(() => {
     cy
       .visit('/')
+    cy
+      .get('[data-cy="login-menu"]')
+      .click();
 
+      cy.get(':nth-child(2) > .LoginModule_logSignSwitch > a').click();
+
+      cy.get('[data-cy=signup-email]')
+      .type('test@user.com');
+
+    cy
+      .get('[data-cy="signup-password"]')
+      .type('pass1234');
+
+    cy
+      .get('[data-cy="signup"]')
+      .click();
+    cy.get('[id="loginMessage"]').should('be.visible');
+    cy.get('[data-cy=logged-user]').click()
+    cy.get('span').click();
+    cy.get('[data-cy="login-menu"]').should('be.visible');
   })
 
-  it.only('test #1', () => {
+  beforeEach(() => {
+    cy
+      .visit('/')
+  })
+
+  after(() => {
+    cy.visit('/')
+    cy.realPress(['F2'])
+    cy.get('#tools > :nth-child(1)').click({ force: true })
+  })
+
+  it('test #1 - no cookie', () => {
 
     cy
       .get('[data-cy="login-menu"]')
@@ -17,20 +46,30 @@ describe('Cookies', () => {
 
     cy
       .get('[data-cy="login-email"]')
-      .type('filip@example.com');
+      .type('test@user.com');
 
     cy
       .get('[data-cy="login-password"]')
-      .type('Asdf.1234#');
+      .type('pass1234');
 
     cy
       .get('[data-cy="login"]')
       .click();
+    cy.get('[id="loginMessage"]').should('be.visible');
 
-    });
-
-  it('test #2', () => {
-
+    // const cookie = cy.getCookie('trello_token').log()
+    // //console.log("want some cookies? ", cookie);
+    cy.getCookie('trello_token')
+      .should('exist')
+      .then((c) => {
+        // save cookie until we need it
+        process.env.TRELLO_TOKEN = c.value;
+      })
   });
 
+  it('test #2', () => {
+    cy.setCookie("trello_token", process.env.TRELLO_TOKEN);
+    cy.reload();
+    cy.get('[id="loginMessage"]').should('be.visible');
+  });
 });
