cb720f2218fe9653ea2043f2225ade1bc67884a2
wilsongumba <wilsongumba@gmail.com>
2023-05-18 00:42:35 +0300

    intercepting network requests

--- cypress/integration/intercept_request_start.js
+++ cypress/integration/intercept_request_start.js
@@ -1,12 +1,40 @@
-/// <reference types="cypress" />
+after(() => {
+  // reset via api
+  cy.request({
+    method: 'POST',
+    url: '/api/reset'
+  })
+});
 
 it('Intercept requests', () => {
+
+  cy
+    .intercept({
+      method: 'POST',
+      url: '/api/boards'
+    }).as('createBoard')
 
   cy
     .visit('/')
 
   cy
-    .get('[data-cy=board-item]')
-    .should('have.length', 0)
+    .get('[data-cy=create-board]')
+    .click()
+
+  cy
+    .get('[data-cy=new-board-input]')
+    .type('launching a rocket{enter}')
+
+  // cy
+  //   .wait('@createBoard')
+  //   .its('response.statusCode')
+  //   .should('eq', 201)
+
+  cy
+    .wait('@createBoard')
+    .then((board) => {
+      expect(board.response.statusCode).to.eq(201)
+      expect(board.request.body.name).to.eq('launching a rocket')
+    })
 
 });
